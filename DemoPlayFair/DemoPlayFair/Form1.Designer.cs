73a328490a96f6b5da0a15d6fa7294f6e8c5eaf6
truongdinhkha <94963294+truongdinhkha@users.noreply.github.com>
2023-12-24 15:20:40 +0700

    done

---
+++ DemoPlayFair/DemoPlayFair/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace DemoPlayFair
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRandomKhoa = new System.Windows.Forms.Button();
+            this.txtNhapMaTran = new System.Windows.Forms.TextBox();
+            this.txtMaTran = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblSokyTu = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDuyChuyen = new System.Windows.Forms.Button();
+            this.btnGiaiMa = new System.Windows.Forms.Button();
+            this.btnMaHoa = new System.Windows.Forms.Button();
+            this.txtXuatVanBan = new System.Windows.Forms.TextBox();
+            this.txtNhapVanBan = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnRandomKhoa);
+            this.groupBox1.Controls.Add(this.txtNhapMaTran);
+            this.groupBox1.Controls.Add(this.txtMaTran);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.groupBox1.Location = new System.Drawing.Point(29, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(531, 247);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Khóa";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(282, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Nhập ma trận:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(6, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ma trận:";
+            // 
+            // btnRandomKhoa
+            // 
+            this.btnRandomKhoa.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandomKhoa.ForeColor = System.Drawing.Color.Black;
+            this.btnRandomKhoa.Location = new System.Drawing.Point(285, 82);
+            this.btnRandomKhoa.Name = "btnRandomKhoa";
+            this.btnRandomKhoa.Size = new System.Drawing.Size(240, 33);
+            this.btnRandomKhoa.TabIndex = 4;
+            this.btnRandomKhoa.Text = "Khóa ngẫu nhiên";
+            this.btnRandomKhoa.UseVisualStyleBackColor = true;
+            this.btnRandomKhoa.Click += new System.EventHandler(this.btnRandomKhoa_Click);
+            // 
+            // txtNhapMaTran
+            // 
+            this.txtNhapMaTran.Location = new System.Drawing.Point(285, 56);
+            this.txtNhapMaTran.Name = "txtNhapMaTran";
+            this.txtNhapMaTran.Size = new System.Drawing.Size(240, 22);
+            this.txtNhapMaTran.TabIndex = 3;
+            this.txtNhapMaTran.TextChanged += new System.EventHandler(this.txtNhapMaTran_TextChanged);
+            // 
+            // txtMaTran
+            // 
+            this.txtMaTran.Enabled = false;
+            this.txtMaTran.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaTran.Location = new System.Drawing.Point(6, 56);
+            this.txtMaTran.Multiline = true;
+            this.txtMaTran.Name = "txtMaTran";
+            this.txtMaTran.Size = new System.Drawing.Size(255, 154);
+            this.txtMaTran.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblSokyTu);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnDuyChuyen);
+            this.groupBox2.Controls.Add(this.btnGiaiMa);
+            this.groupBox2.Controls.Add(this.btnMaHoa);
+            this.groupBox2.Controls.Add(this.txtXuatVanBan);
+            this.groupBox2.Controls.Add(this.txtNhapVanBan);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.groupBox2.Location = new System.Drawing.Point(29, 291);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(531, 181);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mã hóa, Giải mã";
+            // 
+            // lblSokyTu
+            // 
+            this.lblSokyTu.AutoSize = true;
+            this.lblSokyTu.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSokyTu.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSokyTu.ForeColor = System.Drawing.Color.Black;
+            this.lblSokyTu.Location = new System.Drawing.Point(178, 131);
+            this.lblSokyTu.Name = "lblSokyTu";
+            this.lblSokyTu.Size = new System.Drawing.Size(31, 13);
+            this.lblSokyTu.TabIndex = 7;
+            this.lblSokyTu.Text = "(199)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(321, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Văn bản đầu ra:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(6, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Văn bản đầu vào:";
+            // 
+            // btnDuyChuyen
+            // 
+            this.btnDuyChuyen.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDuyChuyen.ForeColor = System.Drawing.Color.Black;
+            this.btnDuyChuyen.Location = new System.Drawing.Point(229, 108);
+            this.btnDuyChuyen.Name = "btnDuyChuyen";
+            this.btnDuyChuyen.Size = new System.Drawing.Size(75, 23);
+            this.btnDuyChuyen.TabIndex = 6;
+            this.btnDuyChuyen.Text = "<<";
+            this.btnDuyChuyen.UseVisualStyleBackColor = true;
+            this.btnDuyChuyen.Click += new System.EventHandler(this.btnDuyChuyen_Click);
+            // 
+            // btnGiaiMa
+            // 
+            this.btnGiaiMa.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiaiMa.ForeColor = System.Drawing.Color.Black;
+            this.btnGiaiMa.Location = new System.Drawing.Point(229, 79);
+            this.btnGiaiMa.Name = "btnGiaiMa";
+            this.btnGiaiMa.Size = new System.Drawing.Size(75, 23);
+            this.btnGiaiMa.TabIndex = 5;
+            this.btnGiaiMa.Text = "Giải mã";
+            this.btnGiaiMa.UseVisualStyleBackColor = true;
+            this.btnGiaiMa.Click += new System.EventHandler(this.btnGiaiMa_Click);
+            // 
+            // btnMaHoa
+            // 
+            this.btnMaHoa.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaHoa.ForeColor = System.Drawing.Color.Black;
+            this.btnMaHoa.Location = new System.Drawing.Point(229, 50);
+            this.btnMaHoa.Name = "btnMaHoa";
+            this.btnMaHoa.Size = new System.Drawing.Size(75, 23);
+            this.btnMaHoa.TabIndex = 4;
+            this.btnMaHoa.Text = "Mã hóa";
+            this.btnMaHoa.UseVisualStyleBackColor = true;
+            this.btnMaHoa.Click += new System.EventHandler(this.btnMaHoa_Click);
+            // 
+            // txtXuatVanBan
+            // 
+            this.txtXuatVanBan.Location = new System.Drawing.Point(320, 52);
+            this.txtXuatVanBan.Multiline = true;
+            this.txtXuatVanBan.Name = "txtXuatVanBan";
+            this.txtXuatVanBan.Size = new System.Drawing.Size(205, 99);
+            this.txtXuatVanBan.TabIndex = 3;
+            // 
+            // txtNhapVanBan
+            // 
+            this.txtNhapVanBan.Location = new System.Drawing.Point(6, 50);
+            this.txtNhapVanBan.Multiline = true;
+            this.txtNhapVanBan.Name = "txtNhapVanBan";
+            this.txtNhapVanBan.Size = new System.Drawing.Size(205, 99);
+            this.txtNhapVanBan.TabIndex = 1;
+            this.txtNhapVanBan.TextChanged += new System.EventHandler(this.txtNhapVanBan_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(45, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(509, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mô phỏng Mã Hóa và Giải Mã theo phương pháp PlayFair";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnMaHoa;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 484);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mã hóa PlayFair";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnRandomKhoa;
+		private System.Windows.Forms.TextBox txtNhapMaTran;
+		private System.Windows.Forms.TextBox txtMaTran;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label lblSokyTu;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnDuyChuyen;
+		private System.Windows.Forms.Button btnGiaiMa;
+		private System.Windows.Forms.Button btnMaHoa;
+		private System.Windows.Forms.TextBox txtXuatVanBan;
+		private System.Windows.Forms.TextBox txtNhapVanBan;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ DemoPlayFair/DemoPlayFair/Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoPlayFair
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		
+		string NhapMaTran = ""; //Chuỗi ma trận nhập vào
+		string BangChuCai; //bảng chữ cái
+		char[,] MaTranKhoa = new char[5, 5]; //ma trận
+		char[,] DiaChiNhiPhan = new char[200, 2]; int DemDiaChi = 0; //địa chỉ từng ký tự trên ma trận theo dạng nhị phân
+		char[,] DiaChiNhiPhanKQ = new char[200, 2];//Địa chỉ của kết quả sau khi giải mã hoặc mã hóa
+		int[] DiaChiNhiPhanCapDoi = new int[4];//cho những nơi của bộ đôi.
+		Random random = new Random();
+
+		public string HamDiaChiNhiPhanKQ()
+		{
+			string ChuoiDiaChiNhiPhanKQ = "";
+
+			for (int i = 0; i < DemDiaChi; i++)
+			{
+				ChuoiDiaChiNhiPhanKQ += DiaChiNhiPhanKQ[i, 0].ToString() + DiaChiNhiPhanKQ[i, 1].ToString();
+			}
+
+			return ChuoiDiaChiNhiPhanKQ;
+		}
+
+
+
+		public void GiaiMa()
+		{
+			for (int i = 0; i < DemDiaChi; i++)
+			{
+				//Tìm địa điểm của bộ đôi.
+				HamDiaChiNhiPhanCapDoi(DiaChiNhiPhan[i, 0], DiaChiNhiPhan[i, 1]);
+
+				if (DiaChiNhiPhanCapDoi[0] == DiaChiNhiPhanCapDoi[2]) // nếu chúng ở trên cùng một dòng,
+				{
+					DiaChiNhiPhanKQ[i, 0] = MaTranKhoa[DiaChiNhiPhanCapDoi[0], (DiaChiNhiPhanCapDoi[1] + 4) % 5];
+					DiaChiNhiPhanKQ[i, 1] = MaTranKhoa[DiaChiNhiPhanCapDoi[2], (DiaChiNhiPhanCapDoi[3] + 4) % 5];
+				}
+				else if (DiaChiNhiPhanCapDoi[1] == DiaChiNhiPhanCapDoi[3]) // nếu chúng ở trong cùng một cột,
+				{
+					DiaChiNhiPhanKQ[i, 0] = MaTranKhoa[(DiaChiNhiPhanCapDoi[0] + 4) % 5, DiaChiNhiPhanCapDoi[1]];
+					DiaChiNhiPhanKQ[i, 1] = MaTranKhoa[(DiaChiNhiPhanCapDoi[2] + 4) % 5, DiaChiNhiPhanCapDoi[3]];
+				}
+				else //Nếu hàng và cột khác nhau,
+				{
+					DiaChiNhiPhanKQ[i, 0] = MaTranKhoa[DiaChiNhiPhanCapDoi[0], DiaChiNhiPhanCapDoi[3]];
+					DiaChiNhiPhanKQ[i, 1] = MaTranKhoa[DiaChiNhiPhanCapDoi[2], DiaChiNhiPhanCapDoi[1]];
+				}
+			}
+		}
+
+		//Playfair mã hóa bộ đôi một cách riêng biệt.
+		public void MaHoa()
+		{
+			for (int i = 0; i < DemDiaChi; i++)
+			{
+				//Tìm DiaChiNhiPhanCapDoi của bộ đôi.
+				HamDiaChiNhiPhanCapDoi(DiaChiNhiPhan[i, 0], DiaChiNhiPhan[i, 1]);
+
+				if (DiaChiNhiPhanCapDoi[0] == DiaChiNhiPhanCapDoi[2]) // aynı satırda iseler,
+				{
+					DiaChiNhiPhanKQ[i, 0] = MaTranKhoa[DiaChiNhiPhanCapDoi[0], (DiaChiNhiPhanCapDoi[1] + 1) % 5];
+					DiaChiNhiPhanKQ[i, 1] = MaTranKhoa[DiaChiNhiPhanCapDoi[2], (DiaChiNhiPhanCapDoi[3] + 1) % 5];
+				}
+				else if (DiaChiNhiPhanCapDoi[1] == DiaChiNhiPhanCapDoi[3]) // aynı stunda iseler,
+				{
+					DiaChiNhiPhanKQ[i, 0] = MaTranKhoa[(DiaChiNhiPhanCapDoi[0] + 1) % 5, DiaChiNhiPhanCapDoi[1]];
+					DiaChiNhiPhanKQ[i, 1] = MaTranKhoa[(DiaChiNhiPhanCapDoi[2] + 1) % 5, DiaChiNhiPhanCapDoi[3]];
+				}
+				else //satır ve stunları farklı ise,
+				{
+					DiaChiNhiPhanKQ[i, 0] = MaTranKhoa[DiaChiNhiPhanCapDoi[0], DiaChiNhiPhanCapDoi[3]];
+					DiaChiNhiPhanKQ[i, 1] = MaTranKhoa[DiaChiNhiPhanCapDoi[2], DiaChiNhiPhanCapDoi[1]];
+				}
+			}
+		}
+
+		//Tìm dòng và trình tự của cặp mong muốn trong ma trận
+		public void HamDiaChiNhiPhanCapDoi(char ch1, char ch2)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					if (MaTranKhoa[i, j] == ch1)
+					{
+						DiaChiNhiPhanCapDoi[0] = i;
+						DiaChiNhiPhanCapDoi[1] = j; 
+					}
+					else if (MaTranKhoa[i, j] == ch2)
+					{
+						DiaChiNhiPhanCapDoi[2] = i;
+						DiaChiNhiPhanCapDoi[3] = j;
+					}
+				}
+			}
+		}
+
+		//Tạo một chuỗi nhị phân của văn bản đầu vào.
+		public void HamDiaChiNhiPhan(string KyTuLoai)
+		{
+			int j = 0;
+
+			for (int i = 0; i < KyTuLoai.Length;)
+			{
+				DiaChiNhiPhan[j, 0] = KyTuLoai[i];
+
+				if (i == KyTuLoai.Length - 1)
+				{   
+					DiaChiNhiPhan[j, 1] = 'w';
+					j++;
+					break;
+				}
+				else if (KyTuLoai[i] != KyTuLoai[i + 1])
+				{
+					DiaChiNhiPhan[j, 1] = KyTuLoai[i + 1];
+					i += 2;
+				}
+				else
+				{
+					DiaChiNhiPhan[j, 1] = 'x';
+					i++;
+				}
+
+				j++;
+			}
+
+			DemDiaChi = j;
+		}
+
+		public void HamNhapMaTran()
+		{
+			int k = 0;
+			int j = 0;
+			int index = 0;
+
+			BangChuCai = "abcdefghiklmnopqrstuvwxyz";
+			int i = BangChuCai.Length;
+
+			while (i > 0)
+			{
+				index = random.Next(0, i); 
+				MaTranKhoa[k, j] = BangChuCai[index]; 
+				BangChuCai = BangChuCai.Remove(index, 1);
+
+				i--;
+				j++;
+
+				if (j % 5 == 0)
+				{
+					k++;
+					j = 0;
+				}
+			}
+		}
+		
+		public void HamMaTranKhoa()
+		{
+			int j = 0;
+			int k = 0;
+			int i = NhapMaTran.Length;
+			int index1 = 0;
+
+			BangChuCai = "abcdefghiklmnopqrstuvwxyz";
+			while (i > 0)
+			{
+				
+				index1 = BangChuCai.IndexOf(NhapMaTran[NhapMaTran.Length - i]);
+				if (index1 >= 0)
+				{
+					string MT = ""+ NhapMaTran[NhapMaTran.Length - i];
+					string TD = "i";
+					if (MT == "j")
+					{
+						MaTranKhoa[k, j] = TD[0];
+					}
+					else
+					{
+						MaTranKhoa[k, j] = NhapMaTran[NhapMaTran.Length - i];
+						
+					}
+					BangChuCai = BangChuCai.Remove(index1, 1);
+					j++;
+				}
+
+				i--;
+
+				if (j == 5)
+				{
+					k++;
+					j = 0;
+				}
+			}
+
+			i = 0;
+			
+			while (i < BangChuCai.Length)
+			{
+				MaTranKhoa[k, j] = BangChuCai[i];
+
+				i++;
+				j++;
+
+				if (j % 5 == 0)
+				{
+					k++;
+					j = 0;
+				}
+			}
+		}
+		
+		public void MaTran()
+		{
+			txtMaTran.Text = "";
+
+			for (int i = 1; i < 6; i++)
+			{
+				for (int j = 1; j < 6; j++)
+				{
+					string MT = ""+MaTranKhoa[i - 1, j - 1];
+					if (MT == "i" || MT =="j")
+					{
+						txtMaTran.Text += "i/j\r\t";
+					}
+					else
+					{
+						txtMaTran.Text += MaTranKhoa[i - 1, j - 1] + "\r\t";
+					}
+				}
+				txtMaTran.Text += "\r\n\r\n";
+			}
+		}
+
+		//kiểm tra các ký tự không xác định trong văn bản.
+		public string LoaiBoKyTuDB(string KyTuLoai)
+		{
+			for (int i = KyTuLoai.Length - 1; i >= 0; i--)
+			{
+				/*if (!(KyTuLoai[i] == '.' || KyTuLoai[i] == ',' ||
+					KyTuLoai[i] == ':' || KyTuLoai[i] == ' ' ||
+					(KyTuLoai[i] >= 97 && KyTuLoai[i] <= 122) ||
+					KyTuLoai[i] == 'ü' || KyTuLoai[i] == 'ö' ||
+					KyTuLoai[i] == 'ç' || KyTuLoai[i] == 'ş' ||
+					KyTuLoai[i] == 'ğ' || KyTuLoai[i] == 'ı'))
+				{
+					KyTuLoai = KyTuLoai.Remove(i, 1);
+				}*/
+				if(KyTuLoai[i]=='j')
+				{
+					KyTuLoai = "i";
+				}
+				else
+				{
+					if (KyTuLoai[i] == ' ')
+					{
+						KyTuLoai = KyTuLoai.Remove(i, 1);
+					}
+					timer1.Start();
+
+
+				}
+			}
+			return KyTuLoai;
+		}
+
+		private void txtNhapMaTran_TextChanged(object sender, EventArgs e)
+		{
+			if (txtNhapMaTran.Text == "")
+			{
+				//txtNhapVanBan.Enabled = false;
+				//txtXuatVanBan.Enabled = false;
+				NhapMaTran = "";
+			}
+			else
+			{
+				//txtNhapVanBan.Enabled = true;
+				//txtXuatVanBan.Enabled = true;
+				txtNhapMaTran.Text = LoaiBoKyTuDB(txtNhapMaTran.Text);
+				txtNhapMaTran.Select(txtNhapMaTran.Text.Length, 0);
+				NhapMaTran = txtNhapMaTran.Text;
+			}
+			HamMaTranKhoa();
+			MaTran();
+		}
+
+		private void btnRandomKhoa_Click(object sender, EventArgs e)
+		{
+			HamNhapMaTran();
+			MaTran();
+		}
+
+		private void txtNhapVanBan_TextChanged(object sender, EventArgs e)
+		{
+			if (txtNhapVanBan.Text == "")
+			{
+				btnGiaiMa.Enabled = false;
+				btnMaHoa.Enabled = false;
+			}
+			else
+			{
+				txtNhapVanBan.Text = LoaiBoKyTuDB(txtNhapVanBan.Text);
+				txtNhapVanBan.Select(txtNhapVanBan.Text.Length, 0);
+				btnGiaiMa.Enabled = true;
+				btnMaHoa.Enabled = true;
+			}
+
+			if (txtNhapVanBan.Text.Length < 200)
+				lblSokyTu.Text = "(" + (199 - txtNhapVanBan.Text.Length).ToString() + ")";
+		}
+
+		private void btnMaHoa_Click(object sender, EventArgs e)
+		{
+			HamDiaChiNhiPhan(txtNhapVanBan.Text);
+			MaHoa();
+			txtXuatVanBan.Text = HamDiaChiNhiPhanKQ();
+		}
+
+		private void btnGiaiMa_Click(object sender, EventArgs e)
+		{
+			txtXuatVanBan.Text = "";
+
+			HamDiaChiNhiPhan(txtNhapVanBan.Text);
+			GiaiMa();
+			txtXuatVanBan.Text = HamDiaChiNhiPhanKQ();
+		}
+
+		private void btnDuyChuyen_Click(object sender, EventArgs e)
+		{
+			txtNhapVanBan.Text = txtXuatVanBan.Text;
+            txtXuatVanBan.Text = "";
+		}
+
+		private void groupBox1_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			HamMaTranKhoa();
+			MaTran();
+	
+			btnGiaiMa.Enabled = false;
+			btnMaHoa.Enabled = false;
+
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+
+		}
+
+        private void lblLoi_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
